62a8fe8fe7f7ca1c64caec13b417730c91452d54
Ryo Utsunomiya <millionsnowliving@gmail.com>
2019-03-09 22:19:37 +0900

    add ex09

---
+++ ch01/ex09/main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+type FakeClient struct{}
+
+func (c *FakeClient) Get(url string) (resp *http.Response, err error) {
+	if !strings.HasPrefix(url, "http://") {
+		return nil, errors.New("url must have http prefix")
+	}
+
+	return &http.Response{
+		Status: "200 OK",
+		Body:   ioutil.NopCloser(bytes.NewBufferString("<html><html>")),
+	}, nil
+}
+
+func TestFetch(t *testing.T) {
+	out := new(bytes.Buffer)
+	Fetch(&FakeClient{}, "example.com", out)
+
+	got := out.String()
+	want := "200 OK\n<html><html>"
+	if got != want {
+		t.Errorf("got\n %v\n want\n %v", got, want)
+	}
+}
