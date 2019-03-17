c082f6fbf3d36ad5afa764812a659c7042bec636
Ryo Utsunomiya <millionsnowliving@gmail.com>
2019-03-17 22:24:54 +0900

    add ex04

---
+++ ch02/ex04/popcount_test.go
@@ -0,0 +1,23 @@
+package popcount
+
+import (
+	"testing"
+)
+
+func BenchmarkPopCount1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCount1(uint64(i))
+	}
+}
+
+func BenchmarkPopCount2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCount2(uint64(i))
+	}
+}
+
+func BenchmarkPopCount3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCount2(uint64(i))
+	}
+}
