093e6463c60b69e3bb704c7a36b28a2267bf9a70
Ryo Utsunomiya <millionsnowliving@gmail.com>
2019-03-28 14:34:10 +0900

    add ex13

---
+++ ch03/ex13/byte.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+const (
+	KB = 1e+3
+	MB = 1e+6
+	GB = 1e+9
+	TB = 1e+12
+	PB = 1e+15
+	EB = 1e+18
+	ZB = 1e+21
+	YB = 1e+24
+)
+
+func main() {
+	fmt.Println(KB)
+	fmt.Println(MB)
+	fmt.Println(GB)
+	fmt.Println(TB)
+	fmt.Println(PB)
+	fmt.Println(EB)
+	fmt.Println(ZB)
+	fmt.Println(YB)
+}
