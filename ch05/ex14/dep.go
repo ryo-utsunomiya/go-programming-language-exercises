f8b5fb47f317de14a3f22b8c114955f3ecfc8ddb
Ryo Utsunomiya <millionsnowliving@gmail.com>
2019-04-23 23:11:56 +0900

    add 5.14

---
+++ ch05/ex14/dep.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+var prereqs = map[string][]string{
+	"algorithms": {"data structures"},
+	"calculus":   {"linear algebra"},
+
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
+
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
+}
+
+func main() {
+	breadthFirst(dep, []string{firstItem()})
+}
+
+func firstItem() string {
+	var first string
+	for item := range prereqs {
+		first = item
+		break
+	}
+	return first
+}
+
+var depth int
+
+func dep(item string) []string {
+	fmt.Printf("%*s%s\n", depth*2, "", item)
+	depth++
+	return prereqs[item]
+}
+
+func breadthFirst(f func(item string) []string, worklist []string) {
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+}
