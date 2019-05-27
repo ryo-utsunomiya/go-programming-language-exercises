62fcb9dfb10efa2db52c8d412d036b8b0f443c5b
Ryo Utsunomiya <millionsnowliving@gmail.com>
2019-05-28 00:05:34 +0900

    8.2

---
+++ ch08/ex02/ftp-client.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	var (
+		port = flag.Int("port", 2222, "port")
+	)
+	flag.Parse()
+	address := fmt.Sprintf("localhost:%d", *port)
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	fmt.Print("ftp>")
+
+	c := bufio.NewReader(conn)
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		_, err := conn.Write([]byte(s.Text() + "\r\n"))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		b, _, err := c.ReadLine()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(b))
+
+		fmt.Print("ftp>")
+	}
+}
---
+++ ch08/ex02/ftp-server.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	var (
+		port = flag.Int("port", 2222, "port")
+	)
+	flag.Parse()
+	address := fmt.Sprintf("localhost:%d", *port)
+
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go server(conn)
+	}
+
+}
+
+func server(c net.Conn) {
+	b := bufio.NewReader(c)
+
+	for {
+		cmd, _, err := b.ReadLine()
+		if err != nil {
+			continue
+		}
+		processCommand(c, string(cmd))
+	}
+}
+
+func processCommand(w io.Writer, cmd string) {
+	switch cmd {
+	case "pwd":
+		pwd, err := os.Getwd()
+		if err != nil {
+			return
+		}
+		mustPrint(w, pwd)
+	default:
+		mustPrint(w, fmt.Sprintf("unknown command: %s", cmd))
+	}
+}
+
+func mustPrint(w io.Writer, text string) {
+	fmt.Println(text)
+	_, err := w.Write([]byte(text + "\r\n"))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
